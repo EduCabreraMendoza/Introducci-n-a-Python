c530f693551715164ee5f995b73ba24716d97ffe
EduCabreraMendoza <edu.cabrera.mendoza@gmail.com>
2022-09-25 21:05:13 -0500

    Programa que muestra el funcionamiento de las estructuras de control anidadas

---
+++ Estructuras de control/estructuras-anidadas.py
@@ -0,0 +1,22 @@
+# Solicita un número 
+from ast import Num
+
+
+num = float(input("Ingresa un número: ")) 
+# condición uno 
+if num> 0: 
+    adjective = " " 
+    if num >= 1000000: 
+        adjective = " muy grande " 
+    elif num >= 1000: 
+        adjective = " grande " 
+    else:
+        adjective = " pequeño "
+    # El resultado es una cadena con el adjetivo y el tipo de número 
+    result = "Es realmente un número" + adjective + "y positivo" 
+elif num < 0: 
+    result = "Es un número negativo" 
+else: 
+    result = "Este número es cero" 
+# Imprime el resultado 
+print(result) 
