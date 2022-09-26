bdd3a4298f31aade5ef55d8dd6072df6e9c06998
EduCabreraMendoza <edu.cabrera.mendoza@gmail.com>
2022-09-25 20:39:14 -0500

    Programa con ejemplificación del funcionamiento de las variables en python

---
+++ Variebles-y-Operadores/variables.py
@@ -0,0 +1,12 @@
+x = 5
+y = 1 + x*5
+z = ("Hola a todos")
+w = input ("ingresa tu color favorito: ")
+edad = int (input ("ingresa tu edad: "))
+peso = float (input ("¿Cuánto pesas? "))
+
+
+#imprime los datos
+print("El valor de x es: ", x)
+print("el valor de y es: ", y)
+print("Tu color favorito es el" ,w,",tienes",edad,"años y pesas",peso,"kg")
